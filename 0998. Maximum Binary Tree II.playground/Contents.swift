e9ba197d146425fbd5186c26da649117c28ddc4e
wangwenbo <wangwenbomx@gmail.com>
2020-12-24 15:59:39 +0800

    add 998. Maximum Binary Tree II solution;

---
+++ 0998. Maximum Binary Tree II.playground/Contents.swift
@@ -0,0 +1,40 @@
+/*
+ 998. Maximum Binary Tree II
+ 
+ We are given the root node of a maximum tree: a tree where every node has a value greater than any other value in its subtree.
+
+ Just as in the previous problem, the given tree was constructed from an list A (root = Construct(A)) recursively with the following Construct(A) routine:
+
+ If A is empty, return null.
+ Otherwise, let A[i] be the largest element of A.  Create a root node with value A[i].
+ The left child of root will be Construct([A[0], A[1], ..., A[i-1]])
+ The right child of root will be Construct([A[i+1], A[i+2], ..., A[A.length - 1]])
+ Return root.
+ Note that we were not given A directly, only a root node root = Construct(A).
+
+ Suppose B is a copy of A with the value val appended to it.  It is guaranteed that B has unique values.
+
+ Return Construct(B).
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) { self.val = val; self.left = left; self.right = right }
+}
+
+class Solution {
+    func insertIntoMaxTree(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        if root == nil {
+            return TreeNode(val)
+        } else if root!.val < val {
+            return TreeNode(val, root, nil)
+        } else {
+            root?.right = insertIntoMaxTree(root?.right, val)
+            return root
+        }
+    }
+}
